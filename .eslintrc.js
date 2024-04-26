877818709680d4df41871918dfd330557f6ef751
Wajid Ali <127591211+WajidAli05@users.noreply.github.com>
2024-04-26 21:29:21 +0500

    implemented lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,43 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+        browser: true,
+        es2021: true,  // Using modern ECMAScript features
+        node: true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended"  // React-specific linting rules
+    ],
+    parser: "@babel/eslint-parser",  // Using Babel's parser for modern syntax, including JSX
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,  // Enabling linting of JSX
+        },
+        ecmaVersion: 12,  // Corresponds to ECMAScript 2021
+        sourceType: "module",
+        requireConfigFile: false  // No need for a babel config file
     },
-    "rules": {
+    plugins: [
+        "react"  // React plugin
+    ],
+    rules: {
         "indent": [
             "error",
-            "tab"
+            2  // Specifies 2 spaces for indentation; change to "tab" if you prefer tabs
         ],
         "linebreak-style": [
             "error",
-            "unix"
+            "unix"  // Ensure line breaks are UNIX style; use "windows" if developing on Windows
         ],
         "quotes": [
             "error",
-            "double"
+            "double"  // Enforces double quotes
         ],
         "semi": [
             "error",
-            "always"
-        ]
+            "always"  // Enforces semicolons at the end of statements
+        ],
+        "react/react-in-jsx-scope": "off",  // Not necessary for React 17 and later with the new JSX transform
+        "no-console": "warn"  // Warns about console usage, can be set to "error" to forbid it completely
     }
 };
